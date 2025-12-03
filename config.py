757560a7b0a565c2e92816e569a3d39261ddfb29
Pomodor12 <167606195+Pomodor12@users.noreply.github.com>
2025-12-03 17:54:42 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+ADMIN_CHAT_ID = -1003264984732  # ID группы, куда бот будет слать напоминания
+INPUT_CHAT_ID = -5012773570  # ID группы, где пользователи добавляют события
